04a7ac43b01899123784536915679d38b6671591
dhiraj <dhiraj7219@gmail.com>
2023-03-09 17:50:24 +0530

    Consumer updated

--- src/main/java/com/rabbitmq/consumer/UserConsumer.java
+++ src/main/java/com/rabbitmq/consumer/UserConsumer.java
@@ -1,4 +1,4 @@
-package com.rabbitmq.publisher;
+package com.rabbitmq.consumer;
 
 
 import lombok.extern.slf4j.Slf4j;
@@ -9,8 +9,9 @@ import org.springframework.stereotype.Service;
 @Slf4j
 public class UserConsumer {
 
-    @RabbitListener(queues = {"${order-queue}"})
-    public void receiveMessageOrder(String message){
-        log.info(String.format("Received message -> %s", message));
-    }
+//    @RabbitListener(queues = {"${order-queue}"})
+//    public void receiveMessageOrder(String message){
+//        log.info(String.format("Received message -> %s", message));
+//    }
+
 }
